765d5dd02cf3b41850bf8ff8c4a5c224360c7a69
LU99IS99 <133922082+LU99IS99@users.noreply.github.com>
2023-10-31 22:01:21 -0700

    99_cli.py

--- 99_cli.py
+++ 99_cli.py
@@ -1,77 +1,56 @@
-from typing import List
+def create_board():
+    return [[" " for _ in range(3)] for _ in range(3)]
 
-
-def make_empty_board() -> List[List[str]]:
-    """Creates a new empty 3x3 Tic-Tac-Toe board."""
-    return [[' ' for _ in range(3)] for _ in range(3)]
-
-
-def show_board(input_board: List[List[str]]):
-    """Prints the Tic-Tac-Toe board."""
-    for row in input_board:
+def print_board(board):
+    for row in board:
         print(" | ".join(row))
-        print("-" * 9)
+        print("-" * 5)
 
-
-def input_move(input_board: List[List[str]]):
-    """Prompts the user for a move and validates the input."""
-    while True:  # Keep asking for input until a valid move is made
+def get_player_move(player, board):
+    while True:
         try:
-            row = int(input("Input row index (0, 1, 2): "))
-            column = int(input("Input column index (0, 1, 2): "))
-            if row < 0 or row > 2 or column < 0 or column > 2:
-                print('Input invalid, should be >= 0 and <= 2')
-            elif input_board[row][column] != ' ':
-                print('Input invalid, position already taken')
+            row = int(input(f"{player}'s turn. Enter the row number (0-2): "))
+            col = int(input(f"{player}'s turn. Enter the column number (0-2): "))
+            if board[row][col] == " ":
+                return row, col
             else:
-                return row, column  # Valid move
-        except ValueError:
-            print('Input invalid, not an integer')
+                print("That spot is taken. Choose another.")
+        except (IndexError, ValueError):
+            print("Invalid input. Try again.")
 
+def check_win(board):
+    lines = [board[0], board[1], board[2],
+             [board[0][0], board[1][0], board[2][0]],
+             [board[0][1], board[1][1], board[2][1]],
+             [board[0][2], board[1][2], board[2][2]],
+             [board[0][0], board[1][1], board[2][2]],
+             [board[2][0], board[1][1], board[0][2]]]
+    for line in lines:
+        if line[0] == line[1] == line[2] != " ":
+            return True
+    return False
 
-def next_player(old: str):
-    """Switches the player turn."""
-    return 'O' if old == 'X' else 'X'
-
-
-def judge_winner(input_board: List[List[str]]) -> str:
-    """Determines if there is a winner or a draw."""
-    for i in range(3):
-        if input_board[i][0] == input_board[i][1] == input_board[i][2] != ' ':
-            return input_board[i][0]
-    for i in range(3):
-        if input_board[0][i] == input_board[1][i] == input_board[2][i] != ' ':
-            return input_board[0][i]
-    if (input_board[0][0] == input_board[1][1] == input_board[2][2] != ' ' or
-            input_board[2][0] == input_board[1][1] == input_board[0][2] != ' '):
-        return input_board[1][1]
-    if all(cell != ' ' for row in input_board for cell in row):
-        return 'Draw'
-    return ''
-
+def check_draw(board):
+    return all(cell != " " for row in board for cell in row)
 
 def play_game():
-    """Starts and manages the Tic-Tac-Toe game."""
-    board = make_empty_board()
-    winner = ''
-    current_player = 'X'
-    show_board(board)
-    while winner == '':
-        print('Next turn for:', current_player)
-        row, column = input_move(board)
-        board[row][column] = current_player
-        show_board(board)
-        winner = judge_winner(board)
-        if winner:
-            break  # Exit the loop if we have a winner or a draw
-        current_player = next_player(current_player)
+    board = create_board()
+    current_player = "X"
+    while True:
+        print_board(board)
+        row, col = get_player_move(current_player, board)
+        board[row][col] = current_player
+        if check_win(board):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        if check_draw(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+        current_player = "O" if current_player == "X" else "X"
 
-    # Announce the result
-    if winner == 'Draw':
-        print("It's a draw!")
-    else:
-        print(f"Player {winner} wins!")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    play_game()
+n__':
     play_game()
